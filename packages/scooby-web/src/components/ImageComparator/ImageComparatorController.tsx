7ad8959a959bf60eb64f9869e599e808d1826b0a
Federico Terzi <federico@animaapp.com>
2022-10-11 22:57:23 +0200

    feat: add some hotkeys

--- packages/scooby-web/src/components/ImageComparator/ImageComparatorController.tsx
+++ packages/scooby-web/src/components/ImageComparator/ImageComparatorController.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 import { ImageComparator, PreferredMode } from "./ImageComparator";
 
 type Props = {
@@ -27,6 +28,11 @@ export type ImageData =
 
 export const ImageComparatorController = (props: Props) => {
   const [mode, setMode] = useState<PreferredMode>("diff");
+
+  useHotkeys("left", () => setMode("expected"));
+  useHotkeys("right", () => setMode("actual"));
+  useHotkeys("d", () => setMode("diff"));
+  useHotkeys("o", () => setMode("overlap"));
 
   return (
     <ImageComparator
