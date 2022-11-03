e6f54b9abf475266c1f48da790af00f07a9032ac
BenAAndrew <benandrew89@gmail.com>
2022-11-03 15:57:50 +0100

    remove other loaders

--- packages/scooby-core/src/loading/index.ts
+++ packages/scooby-core/src/loading/index.ts
@@ -1,7 +1,7 @@
 import { existsSync } from "fs";
 
 import { TestEntry } from "../types";
-import { getLoaderForPath } from "./loaders";
+import { load } from "./loaders/loader";
 
 export async function loadTestEntries(
   path: string,
@@ -11,9 +11,7 @@ export async function loadTestEntries(
     throw new Error(`the given test path does not exist: '${path}'`);
   }
 
-  const loader = await getLoaderForPath(path);
-
-  const entries = await loader.load(path, (fileType = fileType));
+  const entries = await load(path, (fileType = fileType));
 
   if (!entries.length) {
     throw new Error(`no test entries found in path: ${path}`);
---
+++ packages/scooby-core/src/loading/loaders/loader.ts
@@ -0,0 +1,78 @@
+import path from "path";
+import { readdir } from "fs/promises";
+import { TestEntry } from "../../types";
+import { loadOptions } from "../options";
+import { getTestExtension, getTypeForEntry } from "./util";
+
+export const load = async (
+  directory: string,
+  fileType: string = "",
+  relativePath: string = ""
+) => {
+  const entries: TestEntry[] = [];
+  const absolutePath = path.join(directory, relativePath);
+  const dirEntries = await readdir(absolutePath, { withFileTypes: true });
+  const folders = dirEntries
+    .filter((item) => item.isDirectory())
+    .map((folder) => folder.name);
+  const files = dirEntries
+    .filter((item) => !item.isDirectory())
+    .map((file) => file.name);
+
+  for (const folder of folders) {
+    const directoryTests = await load(
+      directory,
+      (fileType = fileType),
+      (relativePath = folder)
+    );
+    entries.push(...directoryTests);
+  }
+
+  let testExtension = fileType;
+  if (!testExtension) {
+    const foundExtension = getTestExtension(files);
+    if (foundExtension) {
+      testExtension = foundExtension;
+    }
+  }
+
+  if (testExtension) {
+    const baseOptionsFile = files.find((entry) => entry === "scooby.json");
+    const baseOptions = baseOptionsFile
+      ? await loadOptions(path.join(absolutePath, "scooby.json"))
+      : undefined;
+
+    const testFiles = files.filter((subEntry) =>
+      subEntry.endsWith(testExtension)
+    );
+    for (const testFile of testFiles) {
+      const name = path.parse(testFile).name;
+
+      const specificOptionsFile = files.find(
+        (entry) => entry === `${name}.scooby.json`
+      );
+      const specificOptions = specificOptionsFile
+        ? await loadOptions(path.join(absolutePath, `${name}.scooby.json`))
+        : undefined;
+
+      const options = {
+        ...baseOptions,
+        ...specificOptions,
+      };
+
+      const absoluteFilePath = path.join(absolutePath, testFile);
+      const id = relativePath
+        ? `${relativePath.replace("/", "-")}-${name}`
+        : name;
+
+      entries.push({
+        id: id,
+        type: getTypeForEntry(testFile),
+        path: absoluteFilePath,
+        relativePath: path.relative(directory, absoluteFilePath),
+        ...(options && Object.keys(options).length > 0 && { options }),
+      });
+    }
+  }
+  return entries;
+};
