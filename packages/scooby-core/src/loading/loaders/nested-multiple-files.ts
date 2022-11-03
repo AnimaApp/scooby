d3a1472f01178759fb00e8a3243791b86f96be4e
BenAAndrew <benandrew89@gmail.com>
2022-11-03 14:42:35 +0100

    log path.parse filename

--- packages/scooby-core/src/loading/loaders/nested-multiple-files.ts
+++ packages/scooby-core/src/loading/loaders/nested-multiple-files.ts
@@ -41,6 +41,7 @@ export const nestedMultipleFilesLoader: Loader = {
     const testFiles = files.filter((subEntry) => subEntry.endsWith(testExtension));
     for (const testFile of testFiles) {
       const name = path.parse(testFile);
+      console.log("NAME", name);
 
       const specificOptionsFile = files.find(
         (entry) => entry === `${name}.scooby.json`
