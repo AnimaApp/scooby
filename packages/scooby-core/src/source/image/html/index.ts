562c3ed14f954811489dd2ea531c0dead3f487d0
Federico Terzi <100688209+federico-terzi-anima@users.noreply.github.com>
2022-11-10 18:19:12 +0100

    feat(core): implement new screenshot logic (#59)

--- packages/scooby-core/src/source/image/html/index.ts
+++ packages/scooby-core/src/source/image/html/index.ts
@@ -1,10 +1,11 @@
 import url from "url";
 import fastq, { queueAsPromised } from "fastq";
+import { Pool } from "generic-pool";
 import { GenerateImageSourcesOptions } from "..";
 
 import { ImageSourceEntry, TestEntry, Viewport } from "../../../types";
-import { Browser, Page } from "puppeteer";
-import { withBrowser, withPage } from "./runtime";
+import { Browser, Page, TimeoutError } from "puppeteer";
+import { withBrowserPool, withPage, withPooledBrowser } from "./runtime";
 import { createTemporaryFile } from "../../../utils/temp";
 
 const MAX_SCREENSHOT_ATTEMPTS = 3;
@@ -13,33 +14,45 @@ export async function generateHTMLImageSources(
   entries: TestEntry[],
   options: GenerateImageSourcesOptions
 ): Promise<ImageSourceEntry[]> {
-  return await withBrowser(async (browser: Browser) => {
-    console.log(`Using browser ${await browser.version()}`);
-    const queue = fastq.promise<
-      Browser,
-      ScreenshotTaskRequest,
-      ScreenshotTaskResult
-    >(browser, takeScreenshotWithRetries, options.maxThreads ?? 4);
-    const generationTasks = createGenerationTasks(entries, queue);
-    const results = await Promise.all(generationTasks);
+  const maxConcurrency = options.maxThreads ?? 4;
 
-    return results.map((result) => {
-      const matchedEntry = entries.find((entry) => entry.id === result.groupId);
-      if (!matchedEntry) {
-        throw new Error("cannot find matching entry for HTML source results");
-      }
+  return withBrowserPool(
+    async (pool) => {
+      await withPooledBrowser(pool, async (browser) => {
+        console.log("Using puppeteer version:", await browser.version());
+      });
 
-      return {
-        type: "image",
-        id: result.id,
-        groupId: result.groupId,
-        path: result.screenshotPath,
-        tags: matchedEntry.options?.tags ?? [],
-        relativePath: matchedEntry.relativePath,
-        metadata: matchedEntry.options?.metadata,
-      };
-    });
-  });
+      const queue = fastq.promise<
+        Pool<Browser>,
+        ScreenshotTaskRequest,
+        ScreenshotTaskResult
+      >(pool, takeScreenshotWithRetries, maxConcurrency);
+      const generationTasks = createGenerationTasks(entries, queue);
+      const results = await Promise.all(generationTasks);
+
+      return results.map((result) => {
+        const matchedEntry = entries.find(
+          (entry) => entry.id === result.groupId
+        );
+        if (!matchedEntry) {
+          throw new Error("cannot find matching entry for HTML source results");
+        }
+
+        return {
+          type: "image",
+          id: result.id,
+          groupId: result.groupId,
+          path: result.screenshotPath,
+          tags: matchedEntry.options?.tags ?? [],
+          relativePath: matchedEntry.relativePath,
+          metadata: matchedEntry.options?.metadata,
+        };
+      });
+    },
+    {
+      maxBrowsers: maxConcurrency,
+    }
+  );
 }
 
 export type ScreenshotTaskRequest = {
@@ -99,17 +112,19 @@ async function takeScreenshotWithRetries(
 ): Promise<ScreenshotTaskResult> {
   // @ts-ignore
   // eslint-disable-next-line @typescript-eslint/no-this-alias
-  const browser: Browser = this;
+  const browserPool: Pool<Browser> = this;
 
   let error;
   for (let attempt = 0; attempt < MAX_SCREENSHOT_ATTEMPTS; attempt++) {
     try {
-      const result = await takeScreenshot(browser, request);
-      return result;
+      return await takeScreenshot(browserPool, request);
     } catch (currentError) {
       console.error(
-        "experienced error while taking screenshot, trying again: " +
-          currentError
+        `experienced error while taking screenshot for target '${
+          request.htmlPath
+        }', trying again (attempt ${
+          attempt + 1
+        }/${MAX_SCREENSHOT_ATTEMPTS}): ` + currentError
       );
       error = currentError;
     }
@@ -119,36 +134,60 @@ async function takeScreenshotWithRetries(
 }
 
 async function takeScreenshot(
-  browser: Browser,
+  browserPool: Pool<Browser>,
   request: ScreenshotTaskRequest
 ): Promise<ScreenshotTaskResult> {
-  return await withPage(browser, async (page: Page) => {
-    await page.setViewport(request.viewport);
-    console.info(`Screenshotting ${request.htmlPath}`);
-    await page.goto(url.pathToFileURL(request.htmlPath).toString(), {
-      waitUntil: "load",
+  return withPooledBrowser(browserPool, async (browser: Browser) => {
+    return withPage(browser, async (page: Page) => {
+      await page.setViewport(request.viewport);
+      console.info(`screenshotting ${request.htmlPath}`);
+      try {
+        await page.goto(url.pathToFileURL(request.htmlPath).toString(), {
+          timeout: 10000,
+        });
+      } catch (error) {
+        if (!(error instanceof TimeoutError)) {
+          throw error;
+        }
+      }
+
+      try {
+        await page.waitForNetworkIdle({ idleTime: 1000, timeout: 10000 });
+      } catch {
+        console.error(
+          `loading ${request.htmlPath} timed out, could not wait for all network requests to be over`
+        );
+      }
+
+      const screenshotPath = await performScreenshotWithTimeout(page);
+
+      console.log("--> completed screenshot for: " + request.htmlPath);
+
+      return {
+        id: request.id,
+        groupId: request.groupId,
+        screenshotPath,
+      };
     });
+  });
+}
 
-    try {
-      await page.waitForNetworkIdle({ idleTime: 1000, timeout: 8000 });
-    } catch {
-      console.error(`Loading ${request.htmlPath} timed out`);
-    }
+async function performScreenshotWithTimeout(page: Page): Promise<string> {
+  const screenshotPath = await createTemporaryFile("screenshot-html-", ".png");
 
-    const screenshotPath = await createTemporaryFile(
-      "screenshot-html-",
-      ".png"
-    );
-    await page.screenshot({
+  await Promise.race([
+    page.screenshot({
       path: screenshotPath,
       type: "png",
       fullPage: true,
-    });
+    }),
+    new Promise((_, reject) =>
+      setTimeout(
+        () => reject(new Error(`screenshot operation timed out`)),
+        10000
+      )
+    ),
+  ]);
 
-    return {
-      id: request.id,
-      groupId: request.groupId,
-      screenshotPath,
-    };
-  });
+  return screenshotPath;
 }
