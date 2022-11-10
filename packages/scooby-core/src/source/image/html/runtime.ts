82dc22d41d24fcbe09eea84387d5d350ff64b241
Federico Terzi <federico@animaapp.com>
2022-11-10 18:11:25 +0100

    feat: implement new screenshot logic

--- packages/scooby-core/src/source/image/html/runtime.ts
+++ packages/scooby-core/src/source/image/html/runtime.ts
@@ -1,18 +1,12 @@
+import GenericPool, { Factory, Pool } from "generic-pool";
 import puppeteer, { Browser, Page } from "puppeteer";
 
-export const withBrowser = async <T>(
-  fn: (browser: Browser) => T
-): Promise<T> => {
-  const browser = await puppeteer.launch({
+function createBrowser(): Promise<Browser> {
+  return puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
     headless: true,
   });
-  try {
-    return await fn(browser);
-  } finally {
-    await browser.close();
-  }
-};
+}
 
 export const withPage = async <T>(
   browser: Browser,
@@ -23,5 +17,49 @@ export const withPage = async <T>(
     return await fn(page);
   } finally {
     await page.close();
+  }
+};
+
+export const browserFactory: Factory<Browser> = {
+  create: function (): Promise<Browser> {
+    return createBrowser();
+  },
+  destroy: async function (browser: Browser) {
+    await browser.close();
+  },
+};
+
+export const withBrowserPool = async <T>(
+  fn: (pool: Pool<Browser>) => T,
+  options: { maxBrowsers: number }
+): Promise<T> => {
+  const pool = GenericPool.createPool(browserFactory, {
+    min: 1,
+    max: options.maxBrowsers,
+  });
+  try {
+    return await fn(pool);
+  } finally {
+    console.log("closing the puppeteer pool...");
+    await pool.drain();
+    await pool.clear();
+  }
+};
+
+export const withPooledBrowser = async <T>(
+  browserPool: Pool<Browser>,
+  fn: (browser: Browser) => T
+): Promise<T> => {
+  const browser = await browserPool.acquire();
+  try {
+    const result = await fn(browser);
+    await browserPool.release(browser);
+
+    return result;
+  } catch (error) {
+    // If an error occurred, we want to destroy the browser instance to
+    // mitigate errors caused by broken sessions
+    await browserPool.destroy(browser);
+    throw error;
   }
 };
