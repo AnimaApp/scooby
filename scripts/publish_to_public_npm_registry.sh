0c8fca09d026a42a5420f5465118586c63f456a7
Federico Terzi <100688209+federico-terzi-anima@users.noreply.github.com>
2023-03-28 12:49:12 +0200

    chore: add public NPM release CI (#102)

---
+++ scripts/publish_to_public_npm_registry.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Patching NPM registry to point to public NPM registry"
+
+git grep -rl 'https://npm.pkg.github.com' packages/ > affected_files.txt
+
+echo "These files will be affected"
+cat affected_files.txt
+
+if [ "$(uname)" == "Darwin" ]; then
+  # macOS
+  cat affected_files.txt | xargs sed -i '' 's,https://npm.pkg.github.com,https://registry.npmjs.org,g'
+else
+  # Linux
+  cat affected_files.txt | xargs sed -i 's,https://npm.pkg.github.com,https://registry.npmjs.org,g'
+fi
+
+echo "Publishing packages"
+
+for package in `ls packages`; do
+  PACKAGE_PATH=packages/$package
+  pushd $PACKAGE_PATH
+
+  yarn publish
+
+  popd
+done
